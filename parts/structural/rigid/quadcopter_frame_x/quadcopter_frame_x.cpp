52e24c7a33bd11e65b686e820ae3433abf4419c9
smenon <smenon@ethz.ch>
2021-10-29 11:22:52 +0200

    Added x and plus type frames

---
+++ parts/structural/rigid/quadcopter_frame_x/quadcopter_frame_x.cpp
@@ -0,0 +1,67 @@
+#include "quadcopter_frame_x.h"
+#include <iostream>
+
+// Setter functions
+
+QuadcopterFrameX ::QuadcopterFrameX() {}
+
+/// Equations of motion for a quadcopter frames
+void QuadcopterFrameX::dynamics(const matrix::Vector3f body_thrust,
+                                const matrix::Vector3f body_torques) {
+
+  // Compute body to inertial frame transformation
+  _R_OB = matrix::Dcm<float>(orientation_);
+
+  // Acceleration
+  acceleration_ =
+      (_R_OB * body_thrust) - gravity_acc - (linear_drag_coeff_ * velocity_);
+
+  // Angular acceleration
+  angular_acceleration_ =
+      inertia_matrix_inverse_ *
+      (body_torques -
+       angular_velocity_.cross(inertia_matrix_ * angular_velocity_) -
+       (angular_drag_coeff_ * angular_velocity_));
+
+  // Plot variables for debugging
+  // std::cout << "Acceleration:" << acceleration_(0) << '\t' <<
+  // acceleration_(1)
+  //           << '\t' << acceleration_(2) << '\n';
+
+  // std::cout << "Angular acceleration:" << angular_acceleration_(0) << '\t'
+  //           << angular_acceleration_(1) << '\t' << angular_acceleration_(2)
+  //           << '\n';
+
+  // const matrix::Vector3<float> air_drag = linear_drag_coeff_ * velocity_;
+  // std::cout << "Drag term:" << air_drag(0) << '\t' << air_drag(1) << '\t'
+  //           << air_drag(2) << '\t' << '\n';
+}
+
+void QuadcopterFrameX::euler_step(const float dt) {
+
+  // Translation
+  position_ = position_ + velocity_ * dt;
+  velocity_ = velocity_ + acceleration_ * dt;
+
+  // Rotation
+  // Compute quaternion derivative
+  orientation_dot_ = matrix::Quatf::expq(0.5f * dt * angular_velocity_);
+  orientation_ = orientation_ * orientation_dot_;
+
+  // Is normalization required here ?
+  orientation_.normalize();
+  angular_velocity_ = angular_velocity_ + angular_acceleration_ * dt;
+
+  // Store update euler angle representation
+  set_euler_orientation();
+
+  // Plot variables for debugging
+  // std::cout << "Position:" << position_(0) << '\t' << position_(1) << '\t'
+  //           << position_(2) << '\n';
+  // std::cout << "Velocity:" << velocity_(0) << '\t' << velocity_(1) << '\t'
+  //           << velocity_(2) << '\n';
+  // std::cout << "Orientation:" << orientation_(0) << '\t' << orientation_(1)
+  //           << '\t' << orientation_(2) << '\t' << orientation_(3) << '\n';
+  // std::cout << "Angular Velocity:" << angular_velocity_(0) << '\t'
+  //           << angular_velocity_(1) << '\t' << angular_velocity_(2) <<'\n';
+}
---
+++ parts/structural/rigid/quadcopter_frame_x/quadcopter_frame_x.h
@@ -0,0 +1,89 @@
+#pragma once
+#include "rigidbody.h"
+
+/// Represents the quadcopter
+class QuadcopterFrameX : public RigidBody {
+
+public:
+  /// Loads the quadcopter properties from the yaml file
+  QuadcopterFrameX();
+
+  // Variables
+protected:
+  // Geometrical Properties ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  /// Linear drag coefficient
+  matrix::Matrix3f linear_drag_coeff_;
+
+  // /// Angular drag coefficient
+  matrix::Matrix3f angular_drag_coeff_;
+
+  /// Distance from the quadcopter's center of mass to the propellor
+  float arm_length_{};
+
+  // Variables for dynamics function ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  /// Rotation matrix - body to inertial frame
+  matrix::Dcmf _R_OB;
+
+  /// No need to computer inverse of inertia matrix in each function call
+  matrix::SquareMatrix<float, 3> inertia_matrix_inverse_;
+
+  // Constants
+private:
+  // Acceleration due to gravity
+  const matrix::Vector3f gravity_acc = matrix::Vector3f(0, 0, 9.81);
+
+public:
+  /// Quadcopter Dynamics
+  void dynamics(const matrix::Vector3f body_thrust,
+                const matrix::Vector3f body_torque);
+
+  void euler_step(const float dt);
+
+  // Rotation only simulation for tuning attitude controller
+  void attitude_tune_euler_step(const float dt);
+
+public:
+  /// Getter function
+  const float &linear_drag_coeff_x() const { return linear_drag_coeff_(0, 0); }
+  const float &linear_drag_coeff_y() const { return linear_drag_coeff_(1, 1); }
+  const float &linear_drag_coeff_z() const { return linear_drag_coeff_(2, 2); }
+
+  /// Getter function
+  const float &angular_drag_coeff_x() const {
+    return angular_drag_coeff_(0, 0);
+  }
+  const float &angular_drag_coeff_y() const {
+    return angular_drag_coeff_(0, 0);
+  }
+  const float &angular_drag_coeff_z() const {
+    return angular_drag_coeff_(0, 0);
+  }
+
+  /// Getter function
+  const float &arm_length() const { return arm_length_; }
+
+public:
+  /// Setter function
+  void set_linear_drag_coeff(const float data[3]) {
+    matrix::Vector3f linear_drag_coeff(data);
+    linear_drag_coeff_ = matrix::diag(linear_drag_coeff);
+  }
+
+  /// Setter function
+  void set_angular_drag_coeff(const float data[3]) {
+    matrix::Vector3f angular_drag_coeff(data);
+    angular_drag_coeff_ = matrix::diag(angular_drag_coeff);
+  }
+
+  /// Setter function
+  void set_arm_length(const float arm_length) { arm_length_ = arm_length; }
+
+  /// Setter function
+  void set_inertia_matrix_inverse() {
+    inertia_matrix_inverse_(0, 0) = 1 / inertia_matrix_(0, 0);
+    inertia_matrix_inverse_(1, 1) = 1 / inertia_matrix_(1, 1);
+    inertia_matrix_inverse_(1, 1) = 1 / inertia_matrix_(1, 1);
+  }
+};
